da238012685ef3a15b90b1a0574e52ba7951ca81
Nguyễn Duy Nhất <nguyenduynhat.250505@gmail.com>
2025-03-29 07:09:11 +0700

    update energy bar, UI How To Play

---
+++ Assets/Script/EnergyBar/HorizontalProgressBar.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using TMPro;
+using System.Collections;
+using UnityEngine;
+namespace MagicPigGames
+{
+    [Serializable]
+    public class HorizontalProgressBar : ProgressBar
+    {
+        /*
+         * Note: The default ProgressBar class is actually a horizontal progress bar. I'm including this as
+         * a separate class to make it more clear that this is the "Horizontal" one, since there will be other
+         * ones for Vertical etc.
+         *
+         * Perhaps in the future there will be additional updates to Progress Bar as well,
+         * though right now, it really is just a horizontal progress bar.
+         */
+
+        public float progress = 0f;
+        private ProgressBar _progressBar;
+
+        public float fillDuration = 10f;
+        private float timer = 0f;
+        public float wait = 5f;
+        private bool isFull = false;
+        private bool isPaused = false;
+
+        public TextMeshProUGUI shieldText;
+        public KeyCode activationKey = KeyCode.F;
+
+        private Coroutine blinkCoroutine; // Lưu trữ Coroutine nhấp nháy
+
+        protected virtual void Start()
+        {
+            _progressBar = GetComponent<ProgressBar>();
+            if (_progressBar == null)
+                Debug.LogError("ProgressBar component is missing!");
+
+            if (shieldText != null)
+                shieldText.gameObject.SetActive(false);
+        }
+
+        protected virtual void Update()
+        {
+            if (_progressBar == null) return;
+
+            if (GameManager.Instance != null && GameManager.Instance.isGameOver)
+            {
+                fillDuration = 10f;
+                timer = 0f;
+                progress = 0f;
+                _progressBar.SetProgress(progress);
+                isFull = false;
+                isPaused = false;
+
+                if (shieldText != null)
+                    shieldText.gameObject.SetActive(false);
+
+                return;
+            }
+
+            if (!isFull && !isPaused)
+            {
+                timer += Time.deltaTime;
+                progress = Mathf.Clamp01(timer / fillDuration);
+                _progressBar.SetProgress(progress);
+
+                if (progress >= 1f)
+                {
+                    isFull = true;
+
+                    // Nếu chưa chạy Coroutine, bắt đầu nhấp nháy
+                    if (blinkCoroutine == null && shieldText != null)
+                        blinkCoroutine = StartCoroutine(BlinkText());
+                }
+            }
+
+            if (isFull && Input.GetKeyDown(activationKey))
+            {
+                ResetProgress();
+            }
+        }
+
+        private IEnumerator BlinkText()
+        {
+            while (isFull)
+            {
+                shieldText.gameObject.SetActive(!shieldText.gameObject.activeSelf);
+                yield return new WaitForSeconds(0.5f); // Nhấp nháy mỗi 0.5 giây
+            }
+        }
+
+        private void ResetProgress()
+        {
+            progress = 0f;
+            _progressBar.SetProgress(0f);
+            timer = 0f;
+            isFull = false;
+            isPaused = true;
+
+            // Dừng nhấp nháy nếu đang chạy
+            if (blinkCoroutine != null)
+            {
+                StopCoroutine(blinkCoroutine);
+                blinkCoroutine = null;
+            }
+
+            if (shieldText != null)
+                shieldText.gameObject.SetActive(false);
+
+            Invoke(nameof(ResumeProgress), wait);
+        }
+
+        private void ResumeProgress()
+        {
+            isPaused = false;
+        }
+
+        private void OnValidate()
+        {
+            if (_progressBar == null)
+                _progressBar = GetComponent<ProgressBar>();
+        }
+    }
+}
+
--- Assets/Script/Menu/MenuController.cs
+++ Assets/Script/Menu/MenuController.cs
@@ -6,10 +6,15 @@ public class MenuController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Button startButton;
+    public Button howToPlayButton;
+    public GameObject howToPlayText1;
+    public GameObject howToPlayText2;
     void Start()
     {
         startButton.onClick.AddListener(StartGame);
-
+        howToPlayButton.onClick.AddListener(ToggleHowToPlay);
+        howToPlayText1.SetActive(false);
+        howToPlayText2.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,6 +25,12 @@ public class MenuController : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("NhanScene"); 
+        SceneManager.LoadScene("NhatScene"); 
+    }
+    public void ToggleHowToPlay()
+    {
+        bool isActive = !howToPlayText1.activeSelf;
+        howToPlayText1.SetActive(isActive);
+        howToPlayText2.SetActive(isActive);
     }
 }
